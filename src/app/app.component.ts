c59ab694c89cfceeea8b7e803a398f51a509e971
Bishop <christianiradukunda6@gmail.com>
2024-10-27 10:18:28 +0200

    [Boilerplate] - uploaded the boilerplate to start from

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'reusable-demo-template';
+
+  private fb = new FormBuilder();
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+  });
 }
